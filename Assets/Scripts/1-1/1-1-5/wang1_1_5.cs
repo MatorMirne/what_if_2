80ab1bd459202bd2a7a6d9e43279227f93e3fb36
ChoiDDoSik <mgs7987@gmail.com>
2022-08-30 21:36:17 +0900

    20220830 1-1-5

---
+++ Assets/Scripts/1-1/1-1-5/wang1_1_5.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class wang1_1_5 : MonoBehaviour
+{
+    SpriteRenderer spriteRenderer;
+
+    public Sprite walk1;
+    public Sprite walk2;
+    public float speed;
+
+    Vector2 start_position;
+    public float time = 0;
+    float timer = 0;
+
+    private void Awake()
+    {
+        spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+        start_position = transform.position;
+    }
+
+    private void Update()
+    {
+        time += Time.deltaTime;
+        timer += Time.deltaTime*speed;
+        
+        Walk();
+
+        if (time> 2.77f)
+        {
+            Debug.Log("씬5완료");
+            SceneManager.LoadScene("1-1-1");
+        }
+
+    }
+    void Walk()
+    {
+        transform.position = start_position + new Vector2(-time * speed, 0);
+
+        // 1초마다 스프라이트 변경
+        if (0 <= timer && timer < 1)
+        {
+            spriteRenderer.sprite = walk1;
+        }
+        else
+        {
+            spriteRenderer.sprite = walk2;
+            if (timer > 2)
+            {
+                timer = 0;
+            }
+        }
+    }
+}
