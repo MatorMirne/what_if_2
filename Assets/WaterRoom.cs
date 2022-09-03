d47d8ba52bab8acd3be2b85a9589180174829444
ChoiDDoSik <mgs7987@gmail.com>
2022-09-03 18:28:01 +0900

    20220903 1-4-2 물

---
+++ Assets/WaterRoom.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterRoom : MonoBehaviour
+{
+    Vector2 pos;
+
+    private void Start()
+    {
+        pos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(pos.x + " " + pos.y);
+        transform.position = new Vector2(pos.x, pos.y + 0.001f);
+        pos = transform.position;
+    }
+}
