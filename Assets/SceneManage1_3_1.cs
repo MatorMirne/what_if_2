2c10ddb41b1ff4ec7ec3abe0d2805f63d01d94f8
ChoiDDoSik <mgs7987@gmail.com>
2022-09-03 17:27:39 +0900

    20220903 1-3-1

---
+++ Assets/SceneManage1_3_1.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManage1_3_1 : MonoBehaviour
+{
+    public GameObject sleep, phoneOff, phoneOn, background;
+    float timer = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if(timer > 1.5 && timer <= 3)
+        {
+            sleep.SetActive(false);
+            phoneOff.SetActive(true);
+        }
+        else if(timer > 3 && timer <= 4.5)
+        {
+            phoneOff.SetActive(false);
+            phoneOn.SetActive(true);
+        }
+        if (timer > 4.5)
+        {
+            SceneManager.LoadScene("1-3-2");
+        }
+    }
+}
