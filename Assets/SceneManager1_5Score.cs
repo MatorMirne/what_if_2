8686087b195a21de45d98bae8167b956c41ee411
MatorMirne <mirnemator@gmail.com>
2022-09-04 15:12:25 +0900

    1-5score 완성

--- Assets/SceneManager1_5Score.cs
+++ Assets/SceneManager1_5Score.cs
@@ -1,10 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SceneManager1_5Score : SceneBase
 {
     public GameObject clear, hole, elec;
+    public string next_scene;
 
     float time = 0;
     bool fadeout = false;
@@ -66,6 +68,28 @@ public class SceneManager1_5Score : SceneBase
                 {
                     FadeOut(elec.transform.GetChild(i).gameObject);
                 }
+            }
+        }
+
+        if (time > 4f)
+        {
+            if (ManagerScript.data_1_5 == 0)
+            {
+                SceneManager.LoadScene(next_scene);
+            }
+            else if (ManagerScript.data_1_5 == 1)
+            {
+                ManagerScript.chapter1_score--; 
+                Debug.Log("(스코어 -1) " + "현재 스코어:" + ManagerScript.chapter1_score);
+
+                SceneManager.LoadScene("1-5-2");
+            }
+            else if (ManagerScript.data_1_5 == 2)
+            {
+                ManagerScript.chapter1_score--;
+                Debug.Log("(스코어 -1) " + "현재 스코어:" + ManagerScript.chapter1_score);
+
+                SceneManager.LoadScene("1-5-2");
             }
         }
     }
