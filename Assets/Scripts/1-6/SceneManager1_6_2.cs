6b64cacb19d7bdc5c01e3704867dbabe201686af
MatorMirne <mirnemator@gmail.com>
2022-09-05 12:07:26 +0900

    1-6-2 길 계속 생기게

---
+++ Assets/Scripts/1-6/SceneManager1_6_2.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneManager1_6_2 : MonoBehaviour
+{
+    public GameObject road_base;
+
+    float time;
+    float create_road_timer;
+
+    void Start()
+    {
+        time = 0;
+    }
+
+    void Update()
+    {
+        time += Time.deltaTime;
+        create_road_timer += Time.deltaTime;
+
+        if (create_road_timer > 1.97f)
+        {
+            Debug.Log("货肺款 road 积己");
+            GameObject road_ = Instantiate(road_base);
+            create_road_timer = 0;
+        }
+    }
+}
