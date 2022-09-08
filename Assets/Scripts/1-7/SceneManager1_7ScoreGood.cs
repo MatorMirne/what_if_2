37bc417f08094fc81ef270e16f6758de9ce57552
MatorMirne <mirnemator@gmail.com>
2022-09-08 21:04:29 +0900

    Chapter1채점표 준비

---
+++ Assets/Scripts/1-7/SceneManager1_7ScoreGood.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManager1_7ScoreGood : SceneBase
+{
+    public GameObject phew;
+    public Sprite phew2;
+
+    bool point1;
+
+    public GameObject sky_back;
+    public GameObject sky_str;
+
+    bool point2;
+
+    float time;
+
+    void Start()
+    {
+        phew.SetActive(false);
+        FadeIn(phew);
+        time = 0;
+        point1 = true;
+    }
+
+    void Update()
+    {
+        time += Time.deltaTime;
+
+        if (time > 2f)
+        {
+            phew.GetComponent<SpriteRenderer>().sprite = phew2;
+        }
+
+        if (time > 4f&& point1)
+        {
+            point1 = false;
+            point2 = true;
+            FadeOut(phew);
+        }
+
+        if(time > 6f && time < 12f)
+        {
+            FadeIn(sky_back);
+        }
+
+        if (time > 8f && time < 12f)
+        {
+            FadeIn(sky_str);
+        }
+
+        if (time > 12f && point2)
+        {
+            point2 = false;
+            FadeOut(sky_back);
+            FadeOut(sky_str);
+        }
+        if (time > 14f)
+        {
+            SceneManager.LoadScene("Chapter1Score");
+        }
+    }
+}
---
+++ Assets/Scripts/SceneManagerChapter1Score.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneManagerChapter1Score : MonoBehaviour
+{
+    public GameObject score1;
+    public GameObject score2;
+    public GameObject score3;
+    public GameObject score4;
+    public GameObject score5;
+    public GameObject score6;
+    public GameObject score7;
+
+    void Update()
+    {
+        
+    }
+}
